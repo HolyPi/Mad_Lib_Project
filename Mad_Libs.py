27ed18a5e4d12c149f20b4f585f8a8d76abac808
Christopher Francisco <franciscochristopher77777@gmail.com>
2019-08-31 10:43:52 -0700

    Checks for letters in the input and checks for -ing when prompted

--- Mad_Libs.py
+++ Mad_Libs.py
@@ -1,6 +1,6 @@
 import sys
 from termcolor import colored, cprint
-unfilled_Story_String = "‘%ingv people?' said %na. 'Of course you find this interesting! But not because of the %a %n! Because you, yourself, will be %ingv %av until you've burned out! We both know who you really are! Please %Pv %av the, just for once! You've %pv before. Could you %Pv for me instead? Then I'll give you a %n. %na would've liked it!"
+unfilled_Story_String = "‘%ingv people?' said %na. 'Of course you find this interesting! But not because of the %a %n! Because you, yourself, will be %ingv %av until you've burned out! We both know who you really are! Please %Pv %av, just for once! %na would really like it! You've %pv before. Could you %Pv for me instead? Then I'll give you a %n."
 unfilled_Story_List = unfilled_Story_String.split(' ')
 part_Of_Speech_Dictionary = {"%ingv": "a verb ending with -ing","%pv":"a past tense verb", "%Pv": "a present verb",
                             "%na":"a name", "%n": "a single noun", "%av": "an adverb", "%a": "an adjective"}
@@ -10,9 +10,21 @@ color_coded_Part_of_speech = {"%ingv": "red","%pv": "blue", "%Pv": "green", "%na
 
 filledStoryList = []
 
+def ends_with_ing(part_of_speech, user_input):
+    if "ing" in part_of_speech:
+        if "ing" in user_input:
+            return True
+        else:
+            return False
+    else:
+        return True
+    
 def get_user_input(part_Of_Speech):
-    userInput = input("Enter " + part_Of_Speech_Dictionary[part_Of_Speech] + " :")
-    return colored(userInput, color_coded_Part_of_speech[part_Of_Speech])
+    user_input = input("Enter " + part_Of_Speech_Dictionary[part_Of_Speech] + ": ")
+    while (not user_input.isalpha() or not ends_with_ing(part_Of_Speech, user_input)):
+        print("Let's try this again!", end = " ")
+        user_input = input("Enter " + part_Of_Speech_Dictionary[part_Of_Speech] + ": ")
+    return colored(user_input, color_coded_Part_of_speech[part_Of_Speech])
 
 def get_segment_prefix(segment, part_of_speech):
     return segment[0:segment.find(part_of_speech)]
@@ -40,11 +52,10 @@ print()
 print ("Color Key:")
 
 for part_of_speech in part_Of_Speech_Dictionary:
-    cprint (part_of_speech, color_coded_Part_of_speech[part_of_speech])
+    cprint (part_Of_Speech_Dictionary[part_of_speech], color_coded_Part_of_speech[part_of_speech])
 
 print()    
-print("Christopher Francisco's Story:")
-print()
+print("Your Story:")
 
 for story_segment in filledStoryList:
     print(story_segment, end = " ")
